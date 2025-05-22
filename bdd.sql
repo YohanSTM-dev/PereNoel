13e17ad085a204a5aab4f21d0c232e6e49626dac
Losange2 <bosslosange@gmail.com>
2025-05-22 10:37:10 +0200

    Update bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -1,51 +1,50 @@
-CREATE TABLE enfants(
-    id int SERIAL PRIMARY KEY,
-    nom varchar(50),
-    prenom varchar(50),
-    age int
-    idadresse int,
-    FOREIGN KEY (idadresse) REFERENCES adresse(id)
+CREATE TABLE adresses (
+    id SERIAL PRIMARY KEY,
+    adresse varchar(200),
+    idtraineau INT,
+    FOREIGN KEY (idtraineau) REFERENCES traineaux(id) ON DELETE CASCADE
 );
 
-CREATE TABLE jouets(
-    id int SERIAL PRIMARY KEY,
-    nom varchar(50)
-    idatelier int,
-    FOREIGN KEY (idatelier) REFERENCES ateliers(id)
+CREATE TABLE traineaux (
+    id SERIAL PRIMARY KEY
 );
 
-CREATE TABLE ateliers(
-    id int SERIAL PRIMARY KEY,
+CREATE TABLE ateliers (
+    id SERIAL PRIMARY KEY,
     nom varchar(50),
     adresse varchar(200)
 );
 
-CREATE TABLE lutins(
-    id int SERIAL PRIMARY KEY,
+CREATE TABLE enfants (
+    id SERIAL PRIMARY KEY,
     nom varchar(50),
     prenom varchar(50),
-    idtraineau int,
+    age INT,
+    idadresse INT,
+    FOREIGN KEY (idadresse) REFERENCES adresses(id)
+);
+
+CREATE TABLE jouets (
+    id SERIAL PRIMARY KEY,
+    nom varchar(50),
+    idatelier INT,
+    FOREIGN KEY (idatelier) REFERENCES ateliers(id)
+);
+
+CREATE TABLE lutins (
+    id SERIAL PRIMARY KEY,
+    nom varchar(50),
+    prenom varchar(50),
+    idtraineau INT,
     FOREIGN KEY (idtraineau) REFERENCES traineaux(id)
 );
 
-CREATE TABLE traineaux(
-    id int SERIAL PRIMARY KEY
+CREATE TABLE rennes (
+    id SERIAL PRIMARY KEY,
+    nom varchar(50)
 );
 
-CREATE TABLE rennes(
-    id int SERIAL PRIMARY KEY,
-    nom varchar(50),
-
-);
-
-CREATE TABLE adresses(
-    id int SERIAL PRIMARY KEY,
-    adresse varchar(200),
-    idtraineau int,
-    FOREIGN KEY (idtraineau) REFERENCES traineaux(id) ON DELETE CASCADE
-);
-
-CREATE TABLE listejouets(
+CREATE TABLE listejouets (
     enfant_id INT NOT NULL,
     jouet_id INT NOT NULL,
     PRIMARY KEY (enfant_id, jouet_id),
@@ -53,10 +52,10 @@ CREATE TABLE listejouets(
     FOREIGN KEY (jouet_id) REFERENCES jouets(id) ON DELETE CASCADE
 );
 
-CREATE TABLE livraison(
+CREATE TABLE livraison (
     traineau_id INT NOT NULL,
     adresse_id INT NOT NULL,
-    heure time,
+    heure TIME,
     PRIMARY KEY (traineau_id, adresse_id),
     FOREIGN KEY (traineau_id) REFERENCES traineaux(id) ON DELETE CASCADE,
     FOREIGN KEY (adresse_id) REFERENCES adresses(id) ON DELETE CASCADE
