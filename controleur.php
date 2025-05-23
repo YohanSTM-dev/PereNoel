b943d5627b7ca306e7dcdc4eec5de71184aca3a8
Losange2 <bosslosange@gmail.com>
2025-05-23 16:45:26 +0200

    Add files via upload

---
+++ controleur.php
@@ -0,0 +1,40 @@
+<?php
+
+include_once __DIR__ . '/php/db.php';
+include_once __DIR__ . 'php/requete.php';
+
+class AppMVC {
+
+    private $dbb;
+    private $requete;
+
+    public function __construct() {
+        $this->dbb = new POO();
+        require_once __DIR__ . '/php/requete.php'; 
+        $this->requete = new Requete();
+    }
+
+    public function pageAcceuil() {
+        include __DIR__ . '/template/PageAcceuil.html';
+        // echo "<h1>Bienvenue sur la page d'accueil (teste) </h1>";    
+    }
+
+    public function pageJouets() {
+        $jouets = $this->requete->getJouets();
+        include __DIR__ . '/template/PageJouets.php';
+        // echo "<h1>teste affisache  </h1>";
+    }
+
+    public function pageEnfantsJouets() {
+         // $liste = $this->dbb->getEnfantsAvecJouets();
+        include __DIR__ . '/template/PageEnfantsJouets.php';
+    }
+    
+    public function pageLettre(){
+
+        include __DIR__ . "/template/PageFormulaire.php";
+    }
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once __DIR__ . '/php/db.php'; 
+require_once __DIR__ . '/controleur.php'; 
+
+
+$bdd = new POO();
+
+
+if (!isset($_GET['page'])) {
+    header("Location: index.php?page=1");
+    exit();
+}
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 1;
+} 
+
+$monapp = new AppMVC();
+
+
+switch ($page) {
+    case 1:
+       
+        $monapp->pageAcceuil();
+        break;
+
+    case 2:
+       
+        $monapp->pageEnfantsJouets();
+        break;
+
+    case 3: 
+        $monapp -> pageJouets();
+        break;
+
+    case 5:
+        $monapp -> pageLettre();
+        break;
+
+    default:
+       
+        $monapp->pageAcceuil();
+        break;
+}
+
+// page acceuil pour 1 
+// page liste des enfants et leurs jouets pour la 2 
+// page des ateliers et les jouets fabriqué 
+// listye des livraisons par traineau 
+// lutins et leurs ateliers
+
+
+?>
+
